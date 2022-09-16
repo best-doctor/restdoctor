a34a3691d3a1b2f509fb700adc2128e84ee0aaf9
Vladislav Katolyk <v.katolyk@bestdoctor.ru>
2022-09-16 18:20:03 +0300

    Bind structlog context

--- restdoctor/utils/structlog.py
+++ restdoctor/utils/structlog.py
@@ -1,4 +1,11 @@
+from __future__ import annotations
+
 try:
     from structlog import get_logger
 except ImportError:
     from logging import getLogger as get_logger  # noqa: F401
+
+try:
+    from structlog.contextvars import bind_contextvars  # noqa: F401
+except ImportError:
+    bind_contextvars = None
