7ae91d1df956b3be7e0c16cac44a44192ed577c9
Alexander Münz <33218362+Westerling13@users.noreply.github.com>
2024-02-12 11:23:25 +0300

    Add support of Enum type for yaml renderers (#151)
    * BANG-435: add support of Enum type for yaml renderers
    
    * BANG-435: clear useless line break
    
    ---------
    
    Co-authored-by: Alexander Munz <a.munz@bestdoctor.ru>

--- tests/test_unit/conftest.py
+++ tests/test_unit/conftest.py
@@ -1,17 +1,21 @@
-from unittest.mock import PropertyMock
+from __future__ import annotations
+
+import enum
 
 import pytest
-
+from django.db.models import TextChoices
 from pytest_factoryboy import register
+
 from restdoctor.rest_framework.pagination import (
-    PageNumberPagination, PageNumberUncountedPagination,
-    CursorUUIDPagination, CursorUUIDUncountedPagination,
+    CursorUUIDPagination,
+    CursorUUIDUncountedPagination,
+    PageNumberPagination,
+    PageNumberUncountedPagination,
 )
-from restdoctor.rest_framework.resources import ResourceViewSet, ResourceView, ResourceBase
+from restdoctor.rest_framework.resources import ResourceBase, ResourceView, ResourceViewSet
 from restdoctor.utils.api_prefix import get_api_path_prefixes
 from tests.factories import MyModelFactory
 from tests.stubs.models import MyModel
-
 
 register(MyModelFactory)
 
@@ -20,6 +24,7 @@ register(MyModelFactory)
 def n_models(my_model_factory):
     def generate_n_models(n_models, **kwargs):
         return my_model_factory.create_batch(n_models, **kwargs)
+
     return generate_n_models
 
 
@@ -59,23 +64,24 @@ def api_prefix() -> str:
 @pytest.fixture()
 def resource_class(settings):
     def with_args(resource_discriminator, resource, base=None):
-
         class Base(base or ResourceViewSet):
             default_discriminative_value = settings.API_RESOURCE_DEFAULT
             resource_discriminative_param = settings.API_RESOURCE_DISCRIMINATIVE_PARAM
 
         resource_class = type(
-            'TestResource', (Base,),
-            {'resource_views_map': {resource_discriminator: resource}},
+            'TestResource', (Base,), {'resource_views_map': {resource_discriminator: resource}}
         )
         return resource_class
+
     return with_args
 
 
 @pytest.fixture()
 def resource_viewset_dispatch(mocker, resource_class):
     def with_args(resource_discriminator, resource_viewset, actions=None):
-        viewset_class = resource_class(resource_discriminator, resource_viewset, base=ResourceViewSet)
+        viewset_class = resource_class(
+            resource_discriminator, resource_viewset, base=ResourceViewSet
+        )
         mocked_dispatch = mocker.patch.object(resource_viewset, 'dispatch')
         view_func = viewset_class.as_view(actions=actions)
 
@@ -100,3 +106,23 @@ def resource_view_dispatch(mocker, resource_class):
 @pytest.fixture()
 def get_discriminant_spy(mocker):
     return mocker.spy(ResourceBase, 'get_discriminant')
+
+
+class TestStringEnum(enum.Enum):
+    TEST = 'test'
+
+
+class TestIntEnum(enum.Enum):
+    TEST = 1
+
+
+class TestBoolEnum(enum.Enum):
+    TEST = True
+
+
+class TestComplexEnum(enum.Enum):
+    TEST = type('Class', (), {})
+
+
+class TestTextChoices(TextChoices):
+    TEST = 'test', 'test'
