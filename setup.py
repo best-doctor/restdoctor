ccad70a7c82fe1988751131fca7b9c9f4d8321eb
Sergei Butkin <s.butkin@bestdoctor.ru>
2024-02-16 01:05:35 +0300

    Remove python 3.12 from test matrix

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
     ],
     packages=find_packages(),
     include_package_data=True,
